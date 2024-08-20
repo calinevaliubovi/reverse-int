10c923152674a74312bad2122ef9d555b1581d5c
calinevaliubovi <calinevaliubovi2@gmail.com>
2024-08-21 00:26:27 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newNum = Math.abs(n);
+    newNum = newNum.toString().split("").reverse().join("");
+    return +newNum;
+};
